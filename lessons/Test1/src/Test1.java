d07b270d65ff2e6ea2227095f1d4b853083674bd
Andrei Markov <noteandmark@gmail.com>
2021-07-22 11:06:24 +0300

    changes on 22.07.21

--- lessons/Test1/src/Test1.java
+++ lessons/Test1/src/Test1.java
@@ -1,10 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import java.awt.BorderLayout;
-import java.awt.event.*;
+public class Test1 {
 
-public class Test1 implements ActionListener {
-
+	int x = 20;
+	int y = 20;
+	
+	JFrame frame;
 	JButton button;
 	
 	public static void main(String[] args) {
@@ -13,20 +17,43 @@ public class Test1 implements ActionListener {
 	}
 		
 		public void go() {
-			JFrame frame = new JFrame();
-			JButton button = new JButton("click me");	
-			button.addActionListener(this);
-			frame.getContentPane().add(BorderLayout.EAST,button);
+			frame = new JFrame();
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			MyDrawPanel drawPanel = new MyDrawPanel();
+			
+			button = new JButton("click me");
+			button.addActionListener((ActionListener) new ButtonListener());
+			
+			frame.getContentPane().add(BorderLayout.EAST,button);
+			frame.getContentPane().add(BorderLayout.CENTER,drawPanel);
+			
 			frame.setSize(300,300);
 			frame.setVisible(true);
-		}
-		
-		public void actionPerformed(ActionEvent event) throws NullPointerException {
-			try {
-				button.setText("I've been clicked!");
-			} catch (Exception ex) {
-				ex.printStackTrace();
+			
+			for (int i = 0; i < 130; i++) {
+				x++;
+				y++;
+				drawPanel.repaint();
+				try {
+					Thread.sleep(50);
+				} catch(Exception ex) {}
 			}
 		}
+		
+		class MyDrawPanel extends JPanel {
+			public void paintComponent(Graphics g) {
+				g.setColor(Color.black);
+				g.fillRect(0, 0, this.getWidth(), this.getHeight());
+				g.setColor(Color.green);
+				g.fillOval(x, y, 40, 40);
+			}
+		}
+		
+		class ButtonListener implements ActionListener {
+			public void actionPerformed(ActionEvent event) {
+				button.setText("I've been clicked!");
+			}
+		}
+
 }
