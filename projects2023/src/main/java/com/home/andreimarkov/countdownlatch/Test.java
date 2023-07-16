f8055cdf3a1b06592cb0dce93f4a859f7a90867d
Andrei Markov <noteandmark@gmail.com>
2023-07-16 16:16:25 +0300

    some refactoring in example

--- projects2023/src/main/java/com/home/andreimarkov/countdownlatch/Test.java
+++ projects2023/src/main/java/com/home/andreimarkov/countdownlatch/Test.java
@@ -10,19 +10,23 @@ public class Test {
 
 		ExecutorService executorService = Executors.newFixedThreadPool(3);
 		for (int i = 0; i < 3; i++) {
-			executorService.submit(new Processor(countDownLatch));
+			executorService.submit(new Processor(i, countDownLatch));
 		}
 		executorService.shutdown();
-		
-		countDownLatch.await();
-		System.out.println("Latch has been opened, main thread is proceeding!");
+		for (int i = 0; i < 3; i++) {
+			Thread.sleep(1000);
+			countDownLatch.countDown();
+		}
 	}
 }
 
 class Processor implements Runnable {
 	private CountDownLatch countDownLatch;
+	private int id;
 
-	public Processor(CountDownLatch countDownLatch) {
+
+	public Processor(int id, CountDownLatch countDownLatch) {
+		this.id = id;
 		this.countDownLatch = countDownLatch;
 	}
 
@@ -33,7 +37,12 @@ class Processor implements Runnable {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		countDownLatch.countDown();
+		try {
+			countDownLatch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Thread with id " + id + " proceded");
 	}
 
 }
