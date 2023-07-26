9d418973e63ed7818f7df4c63eab0452d9f37155
Andrei Markov <noteandmark@gmail.com>
2023-07-26 15:32:57 +0300

    works with hibernate tasks

--- projects2023/src/main/java/com/home/andreimarkov/springJdbcTemplateHibernate/src/main/java/com/andmark/springhibernateexample/models/Person.java
+++ projects2023/src/main/java/com/home/andreimarkov/springJdbcTemplateHibernate/src/main/java/com/andmark/springhibernateexample/models/Person.java
@@ -1,18 +1,28 @@
 package com.andmark.springhibernateexample.models;
 
+
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "person")
 public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Column(name = "name")
     private String name;
 
     @Min(value = 0, message = "Age should be greater than 0")
+    @Column(name = "age")
     private int age;
 
     @NotEmpty(message = "Email should not be empty")
